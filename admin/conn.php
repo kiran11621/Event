e40b6c686deda61f1625499aa56b2479a8ac9d5a
Kiran Maharana <kiran11621@gmail.com>
2021-10-26 19:38:55 +0530

    Delete

--- admin/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $host = "localhost";
-$username = "root";
-$password = "";
-$databaseName = 'lpv_event';
-$conn = mysqli_connect($host,$username,$password,$databaseName);
-    // mysqli_select_db($conn, 'lpv_event');
-?>
